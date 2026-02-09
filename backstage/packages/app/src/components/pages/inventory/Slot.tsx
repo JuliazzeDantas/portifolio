24a756b912691a9869148626b97dc412a35dc8a7
Juliasse <juliassedantasprogramador@gmail.com>
2026-02-09 20:41:31 -0300

    Utilizar pagina default como base

---
+++ backstage/packages/app/src/components/pages/inventory/Slot.tsx
@@ -0,0 +1,22 @@
+type Slot = {
+    name?: string,
+    image?: string, 
+    aspectRatio?: number
+}
+
+export const SlotInventory: React.FC<Slot> = ({name, image, aspectRatio}) => {
+	if (!image || !name || !aspectRatio) {
+		return (
+			<div className='slot'>
+			</div>
+		);
+	}
+	else {
+		return (
+			<div className='slot' style={{flexDirection:'column'}}>
+				<img className="item" src={image} style={{aspectRatio: `1 / ${aspectRatio}`}} />
+				<div className="item-label">{name}</div>
+			</div>
+		);
+	}
+}
