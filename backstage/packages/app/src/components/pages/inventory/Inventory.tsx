f9c77af9997872e55e9455ba8c695f58fe3826e4
Juliasse <juliassedantasprogramador@gmail.com>
2026-02-27 00:59:23 -0300

    Automatizar criação de itens usando entidades

--- backstage/packages/app/src/components/pages/inventory/Inventory.tsx
+++ backstage/packages/app/src/components/pages/inventory/Inventory.tsx
@@ -4,59 +4,15 @@ import React from 'react';
 import '../../core/core-styles/core-page.css';
 import './styles/inventory.css';
 
-import scroll from './images/scroll.png';
-import book from './images/book.png';
-
-import {SlotInventory} from './Slot';
 import { DefaultPage } from '../../core/DefaultPage';
+import { RowGenerator } from './InventoryGenerator';
 
-export const InventoryPage: React.FC= () => { 
+
+export const InventoryPage: React.FC = () => {
 	return (
 		<DefaultPage titleHeader="Inventory">
 			<div className='inventory-container'>
-				<div className='row' style={{ display: 'flex', flexDirection: 'row' }}>
-					<SlotInventory name="Scroll" image={scroll} aspectRatio={0.75} />
-					<SlotInventory name="Book" image={book} aspectRatio={0.6} />
-					<SlotInventory name="Scroll" image={scroll} aspectRatio={0.75} />
-					<SlotInventory />
-						
-				</div>
-				<div className='row' style={{ display: 'flex', flexDirection: 'row' }}>
-					<SlotInventory />
-					<SlotInventory />
-					<SlotInventory />
-					<SlotInventory />
-				</div>
-				<div className='row' style={{ display: 'flex', flexDirection: 'row' }}>
-					<SlotInventory />
-					<SlotInventory />
-					<SlotInventory />
-					<SlotInventory />
-				</div>
-				<div className='row' style={{ display: 'flex', flexDirection: 'row' }}>
-					<SlotInventory />
-					<SlotInventory />
-					<SlotInventory />
-					<SlotInventory />
-				</div>
-				<div className='row' style={{ display: 'flex', flexDirection: 'row' }}>
-					<SlotInventory />
-					<SlotInventory />
-					<SlotInventory />
-					<SlotInventory />
-				</div>
-				<div className='row' style={{ display: 'flex', flexDirection: 'row' }}>
-					<SlotInventory />
-					<SlotInventory />
-					<SlotInventory />
-					<SlotInventory />
-				</div>
-				<div className='row' style={{ display: 'flex', flexDirection: 'row' }}>
-					<SlotInventory />
-					<SlotInventory />
-					<SlotInventory />
-					<SlotInventory />
-				</div>
+				<RowGenerator />
 			</div>
 		</DefaultPage>
 	);
--- backstage/packages/app/src/components/pages/inventory/Slot.tsx
+++ backstage/packages/app/src/components/pages/inventory/Slot.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-
-type Slot = {
+import { useNavigate } from 'react-router-dom';
+export type Slot = {
     name?: string,
     image?: string, 
-    aspectRatio?: number
+    aspectRatio?: number,
+	id?: string,
+	namespace?: string,
 }
 
-export const SlotInventory: React.FC<Slot> = ({name, image, aspectRatio}) => {
-	if (!image || !name || !aspectRatio) {
-		return (
-			<div className='slot'>
-			</div>
-		);
-	}
-	else {
-		return (
-			<div className='slot' style={{flexDirection:'column'}}>
-				<img className="item" src={image} alt={`Inventory item: ${name}`} style={{aspectRatio: `1 / ${aspectRatio}`}} />
-				<div className="item-label">{name}</div>
-			</div>
-		);
-	}
+export const SlotInventory: React.FC<Slot> = ({ name, image, aspectRatio, id, namespace}) => {
+    if (!image || !name || !aspectRatio || !id || !namespace) {
+        return (
+            <div className='slot'>
+            </div>
+        );
+    } else {
+		const navigate = useNavigate();
+		const onCLick = () => {
+			if (image && name && aspectRatio) {
+				navigate(`/catalog/${namespace}/resource/${id}`);
+			}
+		};
+        return (
+            <div className='slot' style={{ flexDirection: 'column', cursor: 'pointer' }} onClick={onCLick}>
+                <img className="item" src={image} alt={`Inventory item: ${name}`} style={{ aspectRatio: `1 / ${aspectRatio}` }} />
+                <div className="item-label">{name}</div>
+            </div>
+        );
+    }
 }
